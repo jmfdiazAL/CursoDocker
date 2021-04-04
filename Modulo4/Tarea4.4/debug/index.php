44bffa2b96fc52107f3ec656e6084614b2cee801
José Manuel Fernández Díaz <josemanuel.fernandez@iesalandalus.org>
2021-04-04 14:12:18 +0200

    04/04/2021

---
+++ Modulo4/Tarea4.4/debug/index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Prueba de PHP</title>
+    </head>
+    <body>
+        <h1>Prueba de PHP</h1>
+        <?php
+            echo "<ul>";
+            for ($i=0; $i<10; $i++) {
+                echo "<li>$i</li>";
+            }
+            echo "</ul>";
+        ?>
+    </body>
+</html>
